a6b86a0ec3e28e2d4b98c1d08df41b0043623d17
silver0V <zelver980535@gmail.com>
2023-04-23 20:30:28 +0800

    ye

--- index.php
+++ index.php
@@ -10,6 +10,16 @@
 	<link href="https://fonts.googleapis.com/css2?family=Tilt+Neon&display=swap" rel="stylesheet">
 	<title>JMV Junkshop!</title>
 	<link rel="icon" href="images/jmvlogotrans.png" sizes="96x96" type="image/png">
+
+	<!-- Vendor CSS Files -->
+	<link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  	<link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  	<link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  	<link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  	<link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  	<!-- Template Main CSS File -->
+  	<link href="assets/css/style.css" rel="stylesheet">
 </head>
 <body>
 	<!-- ======= Header ======= -->

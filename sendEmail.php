e3a0ff3c31a0e695af3812d70a0c3beed4264b51
silver0V <zelver980535@gmail.com>
2023-05-26 22:53:14 +0800

    new4

--- sendEmail.php
+++ sendEmail.php
@@ -1,16 +1,59 @@
 <?php
 
-if (isset($_POST['submit'])){
+// if (isset($_POST['submit'])){
+//     $name = $_POST['name'];
+//     $email = $_POST['email'];
+//     $feedback = $_POST['message'];
+
+//     $mailTo = "jmvjunkshop@gmail.com";
+//     $headers = "Feedback from: " . $name;
+    
+//     mail($mailTo, $name, $feedback, $headers);
+
+//     header("Location: feedback.php");
+// }
+
+if (isset($_POST["submit"])) {
     $name = $_POST['name'];
     $email = $_POST['email'];
-    $feedback = $_POST['message'];
-
-    $mailTo = "jmvjunkshop@gmail.com";
-    $headers = "Feedback from: " . $name;
+    $message = $_POST['message'];
+    // $human = intval($_POST['human']);
+    $from = $email; 
     
-    mail($mailTo, $name, $feedback, $headers);
+    // WARNING: Be sure to change this. This is the address that the email will be sent to
+    $to = 'jmvjunkshop@gmail.com'; 
+    
+    $subject = "Feedback from: ".$name." ";
+    
+    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
 
-    header("Location: feedback.php");
+    // Check if name has been entered
+    if (!$_POST['name']) {
+        $errName = 'Please enter your name';
+    }
+    
+    // Check if email has been entered and is valid
+    if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+        $errEmail = 'Please enter a valid email address';
+    }
+    
+    //Check if message has been entered
+    if (!$_POST['message']) {
+        $errMessage = 'Please enter your message';
+    }
+    //Check if simple anti-bot test is correct
+    // if ($human !== 5) {
+    //     $errHuman = 'Your anti-spam is incorrect';
+    // }
+
+// If there are no errors, send the email
+if (!$errName && !$errEmail && !$errMessage) {
+if (mail ($to, $subject, $body, $from)) {
+    $result='<div class="alert alert-success">Thank You! I will be in touch</div>';
+} else {
+    $result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later</div>';
+}
+}
 }
 
 ?>
